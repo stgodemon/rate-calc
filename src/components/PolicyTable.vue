8c8022a2a4d5d690ef8e6c05e42029cc18c4addc
Dafrok <o.o@mug.dog>
2024-04-20 21:28:15 +0800

    Fix warning

--- src/components/PolicyTable.vue
+++ src/components/PolicyTable.vue
@@ -9,7 +9,7 @@ defineProps({
             <el-text size="large" class="card-header">价格策略</el-text>
         </template>
         <div>
-            <el-table :data="policy" stripe="true" border="true">
+            <el-table :data="policy" stripe border>
                 <el-table-column prop="limit" label="进货价上限（JPY）" width="150" />
                 <el-table-column prop="rate" label="实算汇率" width="120" />
                 <el-table-column prop="fee" label="手续费" />
